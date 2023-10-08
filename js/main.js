146ad3815c8146ef80eeb5d0b57e38b66332c178
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-10-08 11:59:03 +0200

    update sign up page mobile
    add minor js detail to hide validation text, add line element and logo and change alignment

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
     console.log("Hello World")
 
+    // navigate from splash screen to login screen
     $("#btnLogin").click(function(){
         window.location.href = "../pages/login.html";
     });
 
+    // navigate from splash screen to sign up screen
     $("#btnSignUp").click(function(){
         window.location.href = "../pages/signup.html";
     });
 
+    // Hide all the labels on the sign up form for validation
+    $("#UsernameLabel").hide();
+    $("#EmailLabel").hide();
+    $("#PasswordLabel").hide();
+    $("#PasswordConLabel").hide();
 });
