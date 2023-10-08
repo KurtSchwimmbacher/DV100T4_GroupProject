cc22f8a6976f6c4ddc21b1a54e0890b89074e286
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-10-08 13:47:20 +0200

    add basic validation to login
    add very simple validation to login with admin pass

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+// variables
+// ===========================================================================================
+const adminUser = "Admin";
+const adminPass = "Pass";
+
+
 $(document).ready(function(){
     console.log("Hello World")
+
+    // set the validation text to be invisible
+    clearValidationText();
 
     // navigate from splash screen to login screen
     $("#btnLogin").click(function(){
@@ -11,14 +20,44 @@ $(document).ready(function(){
         window.location.href = "../pages/signup.html";
     });
 
+    $("#loginBtn").click(function(){
+        let userIn = $("#UsernameInput").val()+"";
+        let passIn = $("#PasswordInput").val() +"";
+        validateLogin(adminUser,adminPass, userIn, passIn);
+    });
+
+});
+
+// functions
+// ===========================================================================================
+// function to clear validation text
+function clearValidationText(){
+    
     // Hide all the labels on the sign up form for validation
     $("#UsernameLabel").hide();
     $("#EmailLabel").hide();
     $("#PasswordLabel").hide();
     $("#PasswordConLabel").hide();
-});
+}
 
-
-function validateLogin(){
-    
+// validates user login
+function validateLogin(username, password, userIn, passIn){
+    clearValidationText();
+    if(username === userIn && password === passIn){
+        return "enter";
+    }
+    else if (username != userIn && password === passIn){
+        $("#UsernameLabel").text("Incorrect Username");
+        $("#UsernameLabel").show();
+    }
+    else if (username === userIn && password != passIn){
+        $("#PasswordLabel").text("Incorrect Password");
+        $("#PasswordLabel").show();
+    }
+    else if (username != userIn && password != passIn){
+        $("#PasswordLabel").text("Incorrect Password");
+        $("#PasswordLabel").show();
+        $("#UsernameLabel").text("Incorrect Username");
+        $("#UsernameLabel").show();
+    }
 };
