d79ccecb52565249929fd4de433caefacc9d8917
JamesSmithStudent <125651123+JamesSmithStudent@users.noreply.github.com>
2023-10-15 14:25:21 +0200

    Sign Up Validation
    Colors on The sign Up

---
+++ js/main.js
@@ -0,0 +1,86 @@
+// variables
+// ===========================================================================================
+const adminUser = "Admin";
+const adminPass = "Pass";
+let filterCount = 1;
+
+
+$(document).ready(function(){
+    console.log("Hello World")
+    
+
+    // set the validation text to be invisible
+    clearValidationText();
+
+    // hide filters
+    $(".filter-opt-con").hide(); 
+
+    // navigate from splash screen to login screen
+    $("#btnLogin").click(function(){
+        window.location.href = "../pages/login.html";
+    });
+
+    // navigate from splash screen to sign up screen
+    $("#btnSignUp").click(function(){
+        window.location.href = "../pages/signup.html";
+    });
+
+    $("#loginBtn").click(function(){
+        let userIn = $("#UsernameInput").val()+"";
+        let passIn = $("#PasswordInput").val() +"";
+        validateLogin(adminUser,adminPass, userIn, passIn);
+    });
+   
+
+    // show and hide filters
+    $("#filtersIcon").on('click',function(){ 
+        filterCount++;
+        $(".filter-opt-con").toggle();
+        if(filterCount%2 === 0){
+            $("#filtersIcon").prop('src', "../assets/svgs/funnel-fill.svg");
+        }
+        else{
+            $("#filtersIcon").prop('src', "../assets/svgs/funnel.svg");
+        }
+        
+    });
+
+});
+
+// functions
+// ===========================================================================================
+// function to clear validation text
+function clearValidationText(){
+    
+    // Hide all the labels on the sign up form for validation
+    $("#UsernameLabel").hide();
+    $("#EmailLabel").hide();
+    $("#PasswordLabel").hide();
+    $("#PasswordConLabel").hide();
+}
+
+// validates user login
+function validateLogin(username, password, userIn, passIn){
+    clearValidationText();
+    if(username === userIn && password === passIn){
+        return "enter";
+    }
+    else if (username != userIn && password === passIn){
+        $("#UsernameLabel").text("Incorrect Username");
+        $("#UsernameLabel").show();
+    }
+    else if (username === userIn && password != passIn){
+        $("#PasswordLabel").text("Incorrect Password");
+        $("#PasswordLabel").show();
+    }
+    else if (username != userIn && password != passIn){
+        $("#PasswordLabel").text("Incorrect Password");
+        $("#PasswordLabel").show();
+        $("#UsernameLabel").text("Incorrect Username");
+        $("#UsernameLabel").show();
+    }
+};
+
+
+
+
