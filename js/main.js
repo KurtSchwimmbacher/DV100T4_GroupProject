860994ca970f15b551699633dcbcf3aa84332895
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-10-16 21:23:15 +0200

    quick changes
    a few quick changes and comments

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 console.log("hello")
 
+// variables
+// ======================================================================================================
+// to be replaced by API data - Kurt
 const arrPlaylist = [
     {
         name:"The green knight",
@@ -21,11 +24,34 @@ const arrPlaylist = [
    
 ];
 
-//displays shows in console//
-
-loadShows(arrPlaylist);
+// order with other variable sets
+// default sort
 let appliedSort ="allStreams";
 
+//displays shows in console//
+
+// put function calls in document.ready wrapper
+$(document).ready(function (){
+    // like  this - Kurt
+    // array of movies sent to watch List
+    loadShows(arrPlaylist);
+
+    // on click goes in document.ready
+    //onclick event for sort button
+    $("input[name = 'sortRadio']").click(function(){
+        const appliedSort = $(this).attr('value');
+        console.log("Applied Sort: "+appliedSort);
+        displayStreams();
+    });
+
+
+
+});
+
+
+
+// functions 
+// ======================================================================================================
 function loadShows(){
     console.log(arrPlaylist);
 
@@ -37,22 +63,10 @@ function loadShows(){
         $("#soapiesBox").children().eq(index).find("#title").text(programs.name)
         $("#soapiesBox").children().eq(index).find("#description").text(programs.show)
     }
-}
-
-//onclick event//
+};
 
 
-
-$("input[name = 'sortRadio']").click(function(){
-    const appliedSort = $(this).attr('value');
- 
-     console.log(appliedSort);
-     displayStreams();
- })
-
-
-
-function streams(displayStreams){
+function displayStreams(displayStreams){
     
     let streams = [];
 
