00327702b532e6e6e03abfa9e5ad69a8b3eb28fc
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-10-16 22:01:06 +0200

    create new library.js
    begin moving related functions to library.js page

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@
 // ===========================================================================================
 const adminUser = "Admin";
 const adminPass = "Pass";
-let filterCount = 1;
-let movies = [];
-let genres = [];
+
 
 // to be replaced by API data - Kurt
 // for watchlist
@@ -27,84 +25,6 @@ const arrPlaylist = [
    
 ];
 
-const genreList =[
-    {
-        "id": 28,
-        "name": "Action"
-      },
-      {
-        "id": 12,
-        "name": "Adventure"
-      },
-      {
-        "id": 16,
-        "name": "Animation"
-      },
-      {
-        "id": 35,
-        "name": "Comedy"
-      },
-      {
-        "id": 80,
-        "name": "Crime"
-      },
-      {
-        "id": 99,
-        "name": "Documentary"
-      },
-      {
-        "id": 18,
-        "name": "Drama"
-      },
-      {
-        "id": 10751,
-        "name": "Family"
-      },
-      {
-        "id": 14,
-        "name": "Fantasy"
-      },
-      {
-        "id": 36,
-        "name": "History"
-      },
-      {
-        "id": 27,
-        "name": "Horror"
-      },
-      {
-        "id": 10402,
-        "name": "Music"
-      },
-      {
-        "id": 9648,
-        "name": "Mystery"
-      },
-      {
-        "id": 10749,
-        "name": "Romance"
-      },
-      {
-        "id": 878,
-        "name": "Science Fiction"
-      },
-      {
-        "id": 10770,
-        "name": "TV Movie"
-      },
-      {
-        "id": 53,
-        "name": "Thriller"
-      },
-      {
-        "id": 10752,
-        "name": "War"
-      },
-      {
-        "id": 37,
-        "name": "Western"
-      }
-];
 
 
 
