bf2b3ece007c796d2bb783bcc20cade08b72b92d
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-10-17 09:05:50 +0200

    add example code
    add example code for filtering

--- js/list.js
+++ js/list.js
@@ -35,8 +35,26 @@ $(document).ready(function(){
         displayStreams();
     });
 
+
+    $("input[name= 'filterRadio']").click(function(){
+        appliedFilter = $(this).attr('value');
+        console.log("Applied Filter "+ appliedFilter)
+        basicFilterSortTrips();
+    });
+    
+    
+    $("input[name = 'sortRadio']").click( function(){
+        appliedSort = $(this).attr('value');
+        console.log(appliedSort);
+        basicFilterSortTrips();
+    });
+
+
+
 })
 // =======================================
+// Filter buttons
+// ================================================================================================
 
 
 
@@ -58,3 +76,64 @@ function loadShows(){
     }
 };
 
+// ================================================================================================
+function basicFilterSortTrips(){
+    let filterSortTripArr = [];
+
+        //Filter Trips
+        if(appliedFilter !=""){
+            if(appliedFilter == "short"){
+                filterSortTripArr = tripArr.filter(trip =>trip.duration < 6);
+            }
+            else if(appliedFilter == "long"){
+                filterSortTripArr = tripArr.filter(trip =>trip.duration > 5);
+            }
+            else if(appliedFilter == "single"){
+                filterSortTripArr = tripArr.filter(trip =>trip.destinations.split(",").length === 1);
+            }
+            else if(appliedFilter == "multi"){
+                filterSortTripArr = tripArr.filter(trip =>trip.destinations.split(",").length > 1);
+            }
+            else if(appliedFilter == "round"){
+                filterSortTripArr = tripArr.filter(trip =>trip.roundTrip === true);
+            }
+            else if(appliedFilter == "row"){
+                let arr = tripArr.sort(function (a, b) { 
+                     return b - a; 
+                });
+                filterSortTripArr = arr.slice(Math.max(arr.length - 5, 0))
+            }
+        }   
+        else{
+            filterSortTripArr = tripArr;
+        }
+
+        if(appliedSort){
+            if(appliedSort === "low to high"){
+                // sort by price
+                filterSortTripArr = filterSortTripArr.sort((a,b) =>{
+                    return a.price -b.price;    
+                });
+            }
+            else if(appliedSort === "alphabetically"){
+                // sort by alphabetically, a to z
+                filterSortTripArr = filterSortTripArr.sort((a,b) =>{
+                    return a.name.localeCompare(b.name);    
+                });
+            }
+            else if(appliedSort === "date added"){
+                // sort by date added
+                filterSortTripArr = filterSortTripArr.sort((a,b) =>{
+                    let da = new Date(a.addedDate);
+                    let db = new Date(b.addedDate);
+                    
+                    return db-da;
+                });
+            }
+        }
+
+        console.log(filterSortTripArr);
+
+
+        loadTrips(filterSortTripArr);
+}
