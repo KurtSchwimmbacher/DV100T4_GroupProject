db2a1dba713602a5872efce6c9283efcb7f24fcb
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-10-23 13:12:18 +0200

    start work on storing users to local
    start saving users from sign up to local storage and fix password validation

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,6 @@
 
 
 // variables
-let password = $("#passwordInput");
-let confirm_password = $("#confirmPasswordInput");
 
 
 $(document).ready(function(){
@@ -10,26 +8,32 @@ $(document).ready(function(){
 // -----------------------------------------------------------------
 // sign up form code
 // -----------------------------------------------------------------
-
 // validation functions
+
+
+let password = $("#passwordInput");
+let confirm_password = $("#confirmPasswordInput");
+
+
 password.change(function (){
-    if(password.val() != confirm_password.val()) {
-        console.log(password.val())
-        confirm_password.setCustomValidity("Passwords Don't Match");
-      } else {
-        confirm_password.setCustomValidity('');
-      }
+  if(password.val() != confirm_password.val()) {
+    console.log("password changed")
+      confirm_password[0].setCustomValidity("Passwords Don't Match");
+    } else {
+      confirm_password[0].setCustomValidity('');
+    }
 });
 confirm_password.keyup(function (){
-    console.log(password.val())
-    if(password.val() != confirm_password.val()) {
-        console.log(password.val())
-        confirm_password[0].setCustomValidity("Passwords Don't Match");
-      } else {
-        confirm_password[0].setCustomValidity('');
-        console.log(password.val())
-        
-      }
+  console.log("password changing")
+  if(password.val() != confirm_password.val()) {
+      confirm_password[0].setCustomValidity("Passwords Don't Match");
+    } else {
+      confirm_password[0].setCustomValidity('');
+      
+    }
+
+
+
 });
 
 // -----------------------------------------------------------------
@@ -37,22 +41,46 @@ confirm_password.keyup(function (){
 $("#signUpForm").submit(function(event){
 
 
+  event.preventDefault();
 
-    event.preventDefault();
 
+  if(this.checkValidity() === false){
+      event.stopPropagation();
+  }else{
+      // add submission code, like saving to localstorage
 
-    if(this.checkValidity() === false){
-        event.stopPropagation();
-    }else{
-        // add submission code, like saving to localstorage
-        window.location.href = 'pages/browse.html';
-    }
-    $(this).addClass('was-validated');
+      let password = $("#passwordInput").val();
+      let username = $("#UsernameInputSU").val();
+      let email = $("#EmailInputSU").val();
+
+      let profile = {
+        user: username,
+        email : email,
+        pass: password
+      }
+
+      let isIn = false;
+      console.log(profile)
+      let users = JSON.parse(localStorage.getItem("userProfiles"));
+      if(users.length === 0){
+        let toStore = JSON.stringify(profile)
+        localStorage.setItem("userProfiles",toStore);
+      }
+      else{
+        for(i = 0; i < users.length; i++){
+          if(users[i].user === profile.user){
+            isIn = true;
+          }
+        }
+      }
+      window.location.href = '../index.html';
+  }
+  $(this).addClass('was-validated');
+
 
 });
 
 
 });
-
 
 
