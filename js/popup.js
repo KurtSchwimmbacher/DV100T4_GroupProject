947a79ca4f83696389de444dacd4bff51404decd
JamesSmithStudent <125651123+JamesSmithStudent@users.noreply.github.com>
2023-10-31 14:19:10 +0200

    Popup Java
    Add Java For Popup

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+let review = document.getElementById("review");
+
+function openReview(){
+    review.classList.add("openreview");
+}
+function closeReview(){
+    review.classList.remove("openreview");
+}
+
+
