592a7d8328718d88f207c44838d5f94256e5fcf2
JamesSmithStudent <125651123+JamesSmithStudent@users.noreply.github.com>
2023-10-31 17:15:00 +0200

    Late commit

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,13 @@
 let review = document.getElementById("rReview");
 
 function openReview(){
-    review.classList.remove('review');
+    review.classList.remove('rrreview');
     review.classList.add('openreview');
 }
 
 function closeReview(){
     review.classList.remove("openreview");
-    review.classList.add('review');
+    review.classList.add('rrreview');
 }
 
 
