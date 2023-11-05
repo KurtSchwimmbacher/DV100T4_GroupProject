95ef309b55a0cfe582f04b76a7484e37c68b2bbd
JamesSmithStudent <125651123+JamesSmithStudent@users.noreply.github.com>
2023-11-05 17:23:00 +0200

    Update Commit 2

--- js/list.js
+++ js/list.js
@@ -58,7 +58,49 @@ function loadShows(){
     }
 };
 
+function getMovies(movieID){
 
+  const apiURL = `https://api.themoviedb.org/3/movie/${movieID}?api_key=34e9f99aa672c944811b83fab5b6c232`;
+
+  $.ajax({
+      url: apiURL,
+      method: 'GET',
+      dataType: 'json',
+      success: function(data){
+          console.log(data)
+          
+          let imgUrl = "https://image.tmdb.org/t/p/original" + data.backdrop_path;
+
+          const wlCard = $(`
+          <div class="col-xl-4 col-lg-4 col-md-6 col-xs-12">
+          <div class="card" >
+            <img src="${imgUrl}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-text">${data.title}</h5>
+              <div class="btn-grp-wl">
+                <button class="upvote1"></button>
+                <button class="downvote1"></button>  
+                <button class="remove1"></button>
+              </div>
+            </div>
+          </div>
+        </div>`); 
+         
+        $("#watchlistCon").append(wlCard);
+
+        wlCard.on('click',".card-img-top",function(){
+          window.location.href = `singleFilm.html?id=${data.id}`;
+        })
+
+      },
+      error: function(error){
+          // handle as it comes
+      }
+  });
+
+
+
+};
 
 
 
