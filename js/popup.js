3a5b50b3f5f16d2bf7247ad74288874a85d46570
JamesSmithStudent <125651123+JamesSmithStudent@users.noreply.github.com>
2023-11-06 02:10:21 +0200

    Commit

--- js/popup.js
+++ js/popup.js
@@ -2,18 +2,16 @@ $(document).ready(function(){
 
 $("#upvoteOpen").on('click',function(){
     console.log("Works");
-    $("#rReview").addClass("openreview");
-    $("#rReview").removeClass("review")
+    $("#review").addClass("openreview");
+    $("#review").removeClass("review")
 });
 
 $("#closeBtn").on('click',function(){
-    $("#rReview").removeClass("openreview");
-    $("#rReview").addClass("review")
+    $("#review").removeClass("openreview");
+    $("#review").addClass("review")
 })
 
  
 })
 
-$(document).ready(function(){
 
-})
